25556d53d9beea6546d6257fe6d6b689e8601cfb
Taylor Bolin <t.n.bolin@gmail.com>
2016-02-04 15:43:30 -0800

    added prime factors fn

---
+++ script.js
@@ -0,0 +1,16 @@
+// prime factors
+function primeFactors(n){
+  var factors = [], 
+      divisor = 2;
+  
+  while(n>2){
+    if(n % divisor == 0){
+       factors.push(divisor); 
+       n= n/ divisor;
+    }
+    else{
+      divisor++;
+    }     
+  }
+  return factors;
+}
