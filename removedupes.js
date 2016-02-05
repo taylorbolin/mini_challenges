b86c28e11c2d7c6ac7c00fad4c1fd77df2988b29
Taylor Bolin <t.n.bolin@gmail.com>
2016-02-05 09:58:14 -0800

    remove dupes

---
+++ removedupes.js
@@ -0,0 +1,15 @@
+// remove duplicates
+function removeDuplicate(arr){
+  var exists ={},
+      outArr = [], 
+      elm;
+
+  for(var i =0; i<arr.length; i++){
+    elm = arr[i];
+    if(!exists[elm]){
+      outArr.push(elm);
+      exists[elm] = true;
+   }
+  }
+  return outArr;
+}
